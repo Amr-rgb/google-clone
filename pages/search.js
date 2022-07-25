a1f7c0e00012389515695436461d3d0fef121ee5
Amr Taha <amrshiha2@gmail.com>
2022-07-25 17:40:24 +0200

    adding search page

---
+++ pages/search.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+const search = () => {
+  return (
+    <div>
+      <Head>
+        <title>Search Results</title>
+      </Head>
+
+      {/* header */}
+
+      {/* search results */}
+    </div>
+  );
+};
+
+export default search;
