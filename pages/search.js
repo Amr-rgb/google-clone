3a141eac3b0f3c7acc83be49ab1ac1a781d47570
Amr Taha <amrshiha2@gmail.com>
2022-07-26 17:11:42 +0200

    adding search results

--- pages/search.js
+++ pages/search.js
@@ -1,19 +1,47 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { Header } from "../components/Header";
+import { SearchResults } from "../components/SearchResults";
+import Response from "../Response";
 
-const search = () => {
+const search = ({ results }) => {
+  const router = useRouter();
+
+  console.log(results);
+
   return (
     <div>
       <Head>
-        <title>Search Results</title>
+        <title>{router.query.term} - Google Search</title>
       </Head>
 
       {/* header */}
       <Header />
 
       {/* search results */}
+      <SearchResults results={results} />
     </div>
   );
 };
 
 export default search;
+
+export async function getServerSideProps(context) {
+  const API_KEY = process.env.API_KEY;
+  const CONTEXT_KEY = process.env.CONTEXT_KEY;
+
+  const usedummyData = true;
+  const startIndex = context.query.start;
+
+  const data = usedummyData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+      ).then((res) => res.json());
+
+  return {
+    props: {
+      results: data,
+    },
+  };
+}
